af2d4fa3d181070aa4059ed3cd45f6acc7c7261b
Chad Eymard <eymardcm@gmail.com>
2024-02-29 20:03:01 -0500

    Initial Commit

---
+++ Fruits and Vegetables/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fruits and Vegetables
+//
+//  Created by Chad Eymard on 2/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fruits and Vegetables/Fruits_and_VegetablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fruits_and_VegetablesApp.swift
+//  Fruits and Vegetables
+//
+//  Created by Chad Eymard on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Fruits_and_VegetablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
