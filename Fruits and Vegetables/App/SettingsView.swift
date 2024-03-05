9c7cd82965892d67a8b8a4d601fa041572f1607f
Chad Eymard <eymardcm@gmail.com>
2024-03-04 19:50:08 -0500

    settings view

--- Fruits and Vegetables/App/SettingsView.swift
+++ Fruits and Vegetables/App/SettingsView.swift
@@ -37,6 +37,26 @@ struct SettingsView: View {
                     // MARK: - SECTION 2
                     
                     // MARK: - SECTION 3
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
+                    ) {
+                        
+                        
+                        SettingsRowView(name: "Developer", content: "Chad Eymard")
+                        
+                        SettingsRowView(name: "Designer", content: "Robert Petras")
+                        
+                        SettingsRowView(name: "Compatibility", content: "iOS 14")
+                        
+                        SettingsRowView(name: "Website", linkLabel: "SwiftUI Masterclass", linkDestination: "swiftuimasterclass.com")
+                        
+                        SettingsRowView(name: "Twitter", linkLabel: "@RobertPetras", linkDestination: "twitter.com/robertpetras")
+                        
+                        SettingsRowView(name: "SwiftUI", content: "2.0")
+                        
+                        SettingsRowView(name: "Version", content: "1.1.0")
+                        
+                    } //: GROUPBOX
                 } //: VSTACK
                 .navigationBarTitle(Text("Settings"), displayMode: .large)
                 .navigationBarItems(
